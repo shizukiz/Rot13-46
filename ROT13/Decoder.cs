b3aa002d2ab3d742525288a4f49e264d2d6c3a8b
shizuki <116888863+shizukiz@users.noreply.github.com>
2023-02-23 11:24:37 +0100

    Add files via upload

---
+++ ROT13/Decoder.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ROT13
+{
+
+    internal class Decoder:Program
+    {
+        public string Rot13(string eingabe)
+        {
+              
+
+
+            string Alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            eingabe = eingabe.ToUpper();
+            var result = "";
+
+            for (int i = 0; i < eingabe.Length; i++)
+            {
+                var c = eingabe[i];
+
+                var index = Alpha.IndexOf(c.ToString());
+
+                index = (index + 13) % Alpha.Length;
+
+                var zw = Alpha.Substring(index, 1);
+                result = result + zw;
+
+            }
+            
+            
+
+            return result;
+
+
+        }
+
+        public string Rot47(string eingabe)
+        {
+            string Alpha = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+
+
+
+            var result = "";
+
+            for (int i = 0; i < eingabe.Length; i++)
+            {
+                var c = eingabe[i];
+
+                var index = Alpha.IndexOf(c.ToString());
+
+
+                if (index == -1) continue; 
+                    index = (index + 47) % Alpha.Length;
+                
+                
+                    
+                
+
+                var zw = Alpha.Substring(index, 1);
+
+                result = result + zw;
+
+            }
+           
+           
+
+            return result;
+
+        }
+
+
+    }
+
+
+
+}
---
+++ ROT13/Program.cs
@@ -0,0 +1,189 @@
+﻿using System.Diagnostics.Metrics;
+using System.Diagnostics.Tracing;
+using System.Drawing;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Channels;
+
+namespace ROT13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = 1;
+
+            while (counter == 1)
+            {
+                string propt = @"
+                 /$$$$$$$              /$$             /$$    /$$$$$$       /$$ /$$   /$$ /$$$$$$$$
+                | $$__  $$            | $$           /$$$$   /$$__  $$     /$$/| $$  | $$|_____ $$/
+                | $$  \ $$  /$$$$$$  /$$$$$$        |_  $$  |__/  \ $$    /$$/ | $$  | $$     /$$/ 
+                | $$$$$$$/ /$$__  $$|_  $$_/          | $$     /$$$$$/   /$$/  | $$$$$$$$    /$$/  
+                | $$__  $$| $$  \ $$  | $$            | $$    |___  $$  /$$/   |_____  $$   /$$/   
+                | $$  \ $$| $$  | $$  | $$ /$$        | $$   /$$  \ $$ /$$/          | $$  /$$/    
+                | $$  | $$|  $$$$$$/  |  $$$$/       /$$$$$$|  $$$$$$//$$/           | $$ /$$/     
+                |__/  |__/ \______/    \___/        |______/ \______/|__/            |__/|__/     
+
+                                    Welchen Rot wollen sie En oder Decoden?
+";
+                
+            var converter = new Decoder();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(propt);
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Thread.Sleep(1000);
+            Console.WriteLine();
+            Console.WriteLine("[1] Rot13 (nur buchstaben)");
+            Console.WriteLine("[2] Rot47 (sonderzeichen Buchstaben und Zahlen)");
+            Console.WriteLine("[3] Was ist ein Rot?");
+            Console.WriteLine("[4] Exit");
+
+            Console.WriteLine();
+            Console.Write("Eingabe: ");
+            var eg = Console.ReadLine();
+
+            int abc = Int32.Parse(eg);
+            Thread.Sleep(800);
+
+            Console.Write("Loading...");
+
+            Thread.Sleep(2000);
+            Console.Clear();
+
+                if (abc == 1)
+                {
+                    Console.Write("Eingabe: ");
+                    var eingabe = Console.ReadLine();
+                    var t1 = converter.Rot13(eingabe);
+                    Console.WriteLine("Dein Ergebnis Lautet: " + t1);
+                    Thread.Sleep(2000);
+                    Console.WriteLine();
+                    Console.WriteLine("Möchten sie etwas anderes En oder Decoden?");
+                    Console.WriteLine("[1] für Ja");
+                    Console.WriteLine("[2] um das Programm zu Beenden");
+                    string goback = Console.ReadLine();
+                    int gobackint = int.Parse(goback);
+
+                    if (gobackint == 1)
+                    {
+                        
+                        Console.WriteLine("Sie können in 5 Sekunden ihre Eingabe Erneuern!");
+
+
+                        Thread.Sleep(5000);
+                        Console.Clear();
+
+                    }
+
+                    if (gobackint == 2)
+                    {
+                        Console.WriteLine("Das Programm wird in wenigen Sekunden geschlossen!");
+                        Thread.Sleep(4000);
+                        counter = 0;
+                        return;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                    }
+
+                }
+
+                if (abc == 2)
+                {
+
+                    Console.Write("Eingabe: ");
+                    var eingabe = Console.ReadLine();
+                    var t2 = converter.Rot47(eingabe);
+                    Console.WriteLine("Dein Ergebnis Lautet: " + t2);
+                    Thread.Sleep(3000);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Möchten sie etwas anderes En oder Decoden?");
+                    Console.WriteLine("[1] für Ja");
+                    Console.WriteLine("[2] um das Programm zu Beenden");
+                    
+                    string goback = Console.ReadLine();
+                    int gobackint = int.Parse(goback);
+
+                    if (gobackint == 1)
+                    {
+                        ;
+                        Console.WriteLine("Sie können in 5 Sekunden ihre Eingabe Erneuern!");
+                    
+                    
+                    Thread.Sleep(5000);
+                    Console.Clear();
+
+                    }
+
+                    if (gobackint == 2)
+                    {
+                        Console.WriteLine("Das Programm wird in wenigen Sekunden geschlossen!");
+                        Thread.Sleep(4000);
+                        counter = 0;
+                        return;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                    }
+
+                }
+
+                if (abc == 3)
+                {
+                    Console.WriteLine(
+                        "ROT13 ist eine Caesar-Verschlüsselung, mit der auf einfache Weise Texte verschlüsselt werden können.");
+
+                    Console.WriteLine("Der unterschied Zwischen den Beiden liegt in der Verschiebung Rot13 arbeitet mit einer 13 als\nverschiebung und Rot47 mit einer 47 und mehr Zeichen."+
+                                      "Dazu besitzt Rot 13 nur Buchstaben und\nRot47 Zahlen, Buchstaben und Sonderzeichen.");
+                    Thread.Sleep(3000);
+                    Console.WriteLine();
+                    Console.ReadKey();
+                    Console.WriteLine("Möchten sie etwas anderes En oder Decoden?");
+                    Console.WriteLine("[1] für Ja");
+                    Console.WriteLine("[2] um das Programm zu Beenden");
+
+                    string goback = Console.ReadLine();
+                    int gobackint = int.Parse(goback);
+
+                    if (gobackint == 1)
+                    {
+                        ;
+                        Console.WriteLine("Sie können in 5 Sekunden ihre Eingabe Erneuern!");
+
+
+                        Thread.Sleep(5000);
+                        Console.Clear();
+
+                    }
+                    else
+                    {
+                        Console.Clear();
+                    }
+
+                    if (gobackint == 2)
+                    {
+                        Console.WriteLine("Das Programm wird in wenigen Sekunden geschlossen!");
+                        Thread.Sleep(4000);
+                        counter = 0;
+                        return;
+                    }
+                }
+
+                if (abc == 4)
+                {
+                    return;
+                }
+
+
+            }
+
+
+
+        }
+
+    }
+}
